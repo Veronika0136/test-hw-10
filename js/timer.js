4e8ee81e677b360a1529c458ae8d040c5e0148e2
Veronika0136 <Artemova331@gmail.com>
2025-02-23 00:48:49 +0200

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -3,15 +3,20 @@ const refs = {
     inputData : document.querySelector('#datetime-picker')
 }
 
-// const options = {
-//     enableTime: true,
-//     time_24hr: true,
-//     defaultDate: new Date(),
-//     minuteIncrement: 1,
-//     onClose(selectedDates) {
-//       console.log(selectedDates[0]);
-//     },
-//   };
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+
+  flatpickr("#datetime-picker", options);  
+
+  let userSelectedDate = 0;
 
 // function convertMs(ms) {
 //     // Number of milliseconds per unit of time
