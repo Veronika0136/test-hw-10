24665e83fd62f6445986da26afdb0a2e9546fd18
Veronika0136 <Artemova331@gmail.com>
2025-02-23 20:23:35 +0200

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,10 +1,10 @@
 const refs = {
   btn: document.querySelector("[data-start]"),
-  input: document.querySelector('#datetime-picker'),
-  day: document.querySelector('span[data-days]'),
-  hour: document.querySelector('span[data-hours]'),
-  minute: document.querySelector('span[data-minutes]'),
-  second: document.querySelector('span[data-seconds]'),
+  input: document.querySelector("#datetime-picker"),
+  day: document.querySelector("span[data-days]"),
+  hour: document.querySelector("span[data-hours]"),
+  minute: document.querySelector("span[data-minutes]"),
+  second: document.querySelector("span[data-seconds]"),
 };
 
 refs.btn.disabled = true;
@@ -17,22 +17,21 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0] > Date.now()) {
-      userSelectedDate = selectedDates[0]
-      console.log(userSelectedDate);
+      userSelectedDate = selectedDates[0];
+
       refs.btn.classList.add("active");
       refs.btn.disabled = false;
-      
     } else {
       iziToast.error({
-        title: 'Error',
-        message: 'Please choose a date in the future',
-        position: 'topRight',
-        backgroundColor:'#EF4040',
-        titleColor:'white',
-        messageColor:'white',
-    });
+        title: "Error",
+        message: "Please choose a date in the future",
+        position: "topRight",
+        backgroundColor: "#EF4040",
+        titleColor: "white",
+        messageColor: "white",
+      });
       // window.alert("Please choose a date in the future");
-    };
+    }
   },
 };
 
@@ -43,7 +42,6 @@ const timer = {
   initTime: null,
 
   start() {
-    console.log("start");
     this.initTime = userSelectedDate;
     this.intervalId = setInterval(() => {
       this.tick();
@@ -54,28 +52,19 @@ const timer = {
   },
 
   stop() {
-    console.log("stop");
     clearInterval(this.intervalId);
-    refs.btn.disabled = false;
     refs.input.disabled = false;
-    
   },
 
   tick() {
     const currentTime = Date.now();
     const diff = this.initTime - currentTime;
     const time = convertMs(diff);
-    const resultTime = timeToStr(time);
-    let arrTime = resultTime.split(' ');
-    console.log(resultTime);
-    console.log(arrTime);
-    refs.day.textContent = arrTime[0];
-    refs.hour.textContent = arrTime[1];
-    refs.minute.textContent = arrTime[2];
-    refs.second.textContent = arrTime [3];
-    
-    
-
+    refs.day.textContent = time.days;
+    refs.hour.textContent = time.hours;
+    refs.minute.textContent = time.minutes;
+    refs.second.textContent = time.seconds;
+    refs.btn.disabled = true;
     if (diff < 1000) {
       this.stop();
     }
@@ -85,36 +74,31 @@ const timer = {
 refs.btn.addEventListener("click", () => {
   timer.start();
   refs.btn.classList.remove("active");
-  refs.btn.disabled = true;
-  
-
 });
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
-    return { days, hours, minutes, seconds };
-  }
-
-function timeToStr({days, hours, minutes, seconds }){
-  days = days.toString().padStart(2,"0");
-  hours = hours.toString().padStart(2,"0");
-  minutes = minutes.toString().padStart(2,"0");
-  seconds = seconds.toString().padStart(2,"0");
-
-  return `${days} ${hours} ${minutes} ${seconds}`;
+  return { days, hours, minutes, seconds };
 }
 
+function addLeadingZero(value) {
+  value = value.toString().padStart(2, "0");
+
+  return `${value}`;
+}
